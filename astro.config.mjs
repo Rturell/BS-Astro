aa7afb4975c6b95df4aa1e17cbc606e366f263f9
rodrigoturell <rodrigoturell@gmail.com>
2025-12-19 13:26:14 -0300

    feat: update dependencies and add authentication with Clerk
    - Added new dependencies: @astrojs/node, @astrojs/react, @clerk/astro, @clerk/localizations, react, and react-dom.
    - Updated Footer component to dynamically display the current year.
    - Enhanced Header component with authentication features using Clerk.
    - Created middleware for protected routes with Clerk.
    - Updated Layout styles for better spacing.
    - Refactored Apoyos page for improved layout and design.
    - Added new Videos page for exclusive content.
    - Created Smokers Lounge page with restricted access for supporters.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+import clerk from '@clerk/astro'
+import node from '@astrojs/node'
+import {
+  esUY
+} from '@clerk/localizations'
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte(), clerk({
+    localization: esUY,
+  })],
+    output: 'server',
+    adapter: node({
+        mode: 'standalone'
+    }),
 });
